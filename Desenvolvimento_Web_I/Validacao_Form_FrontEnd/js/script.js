fe03ecbe88d69370048f2fba5153b5199fc4d858
Montival Junior <montival_junior@yahoo.com.br>
2020-06-11 07:51:20 -0300

    Adicionada imagem ao index.html

--- Desenvolvimento_Web_I/Validacao_Form_FrontEnd/js/script.js
+++ Desenvolvimento_Web_I/Validacao_Form_FrontEnd/js/script.js
@@ -1,16 +1,8 @@
 //Declaração das Variáveis globais
 let botaoEnviar = null;
 let form = null;
-let inputAnalise = null;
-let inputBanco = null;
-let inputData = null;
-let inputDesenvolvimento = null;
-let inputEngenharia = null;
 let inputEmail = null;
 let inputName = null;
-let inputRedes = null;
-let inputSexoMasculino = null;
-let inputSexoFeminino = null;
 let inputEstadoCivil = null;
 let inputDia = null;
 let inputMes = null;
@@ -34,18 +26,11 @@ window.addEventListener('load', () => {
   function carregaElementos() {
     botaoEnviar = document.querySelector('#botaoEnviar');
     form = document.querySelector('form');
-    inputAnalise = document.querySelector('#inputAnalise');
-    inputBanco = document.querySelector('#inputDatabase');
     inputDia = document.querySelector('#inputDay');
     inputMes = document.querySelector('#inputMonth');
     inputAno = document.querySelector('#inputYear');
-    inputDesenvolvimento = document.querySelector('#inputDevelopment');
-    inputEngenharia = document.querySelector('#inputSoftware');
     inputEmail = document.querySelector('#inputEmail');
     inputName = document.querySelector('#inputName');
-    inputRedes = document.querySelector('#inputRedes');
-    inputSexoMasculino = document.querySelector('#inputMan');
-    inputSexoFeminino = document.querySelector('#inputWoman');
     inputEstadoCivil = document.querySelector('#inputMaritalStatus');
   }
 });
@@ -58,6 +43,7 @@ const apertouBotaoEnviar = (event) => {
   validaCampos();
 
   //Função de captura do resultado Estado Civil
+
   function resCivil(res) {
     if (res === '1') {
       return 'Solteiro';
@@ -72,7 +58,9 @@ const apertouBotaoEnviar = (event) => {
   //Função de validação de Campos
 
   function validaCampos() {
-    if (user === '' || user.length < 15) {
+    if (user === '') {
+      window.alert('Você precisa digitar um nome');
+    } else if (user.length < 15) {
       window.alert(
         'Você precisa digitar um nome válido com no mínimo 15 caracteres'
       );
@@ -84,21 +72,20 @@ const apertouBotaoEnviar = (event) => {
       userEmail == '' ||
       userEmail == null
     ) {
-      alert('Por favor, indique um e-mail válido.');
+      alert('Por favor, indique um e-mail válido. Não esqueça do @ e do .');
       inputEmail.focus();
-      return false;
     } else if (inputDia.value < 1 || inputDia.value > 31 || !inputDia.value) {
-      alert('Por favor, uma data válida.');
+      alert('Por favor, uma data válida. (DIA)');
       inputDia.focus();
     } else if (inputMes.value < 1 || inputMes.value > 12 || !inputMes.value) {
-      alert('Por favor, uma data válida.');
+      alert('Por favor, uma data válida. (MÊS)');
       inputMes.focus();
     } else if (
       inputAno.value < 1920 ||
       inputAno.value > 2020 ||
       !inputAno.value
     ) {
-      alert('Por favor, uma data válida.');
+      alert('Por favor, uma data válida. (ANO)');
       inputAno.focus();
     } else if (
       resCivil(inputEstadoCivil.value) === 'Solteiro' &&
@@ -106,30 +93,8 @@ const apertouBotaoEnviar = (event) => {
     ) {
       alert('Solteiros são aceitos apenas maiores de 15 anos');
       inputAno.focus();
-    } else {
-      window.alert('Dados enviados');
-      //alertaMessage();
-    }
-  }
-  function alertaMessage() {
-    let message =
-      'Nome = ' +
-      user +
-      '\n' +
-      'Email =' +
-      userEmail +
-      '\n' +
-      'Data de Nascimento = ' +
-      inputDia.value +
-      '/' +
-      inputMes.value +
-      '/' +
-      inputAno.value +
-      '\n' +
-      'Estado Civil = ' +
-      resCivil(inputEstadoCivil.value) +
-      ' \n Áreas de Interesse = ';
-    alert(message);
+      return false;
+    } else window.alert('Dados enviados');
   }
 };
 
